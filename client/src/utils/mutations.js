7f4c199559a30f5e4b755ed3d9e054e698614c7e
bcbridges <bcb35b@gmail.com>
2021-10-03 17:38:43 -0600

    Added add user and save book mutation.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,8 +11,32 @@ mutation login($email: String!, $password: String!) {
     }
 }`
 
-export const ADD_USER
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+}`
 
-export const SAVE_BOOK
+export const SAVE_BOOK = gql`
+mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            authors
+            image
+            description
+            title
+            link
+        }
+    }
+}`
 
 export const REMOVE_BOOK

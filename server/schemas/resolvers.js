ec24fd4b0c925fd72b1d780a3573a621eb92d436
bcbridges <bcb35b@gmail.com>
2021-10-03 16:32:50 -0600

    Added saveBook mutation.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -38,7 +38,19 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    saveBook: {},
+    saveBook: async (parent, { bookData }, context) => {
+      if (context.user) {
+        const addedBook = await User.findByIdAndUpdate(
+          {
+            _id: context.user._id,
+          },
+          { $push: { savedBooks: bookData } },
+          { new: true }
+        );
+
+        return addedBook;
+      }
+    },
     removeBook: {},
   },
 };
